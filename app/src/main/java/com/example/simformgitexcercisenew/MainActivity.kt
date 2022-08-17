a03b2bf84c65f934a344cfa061d6ade98940a039
Jitendra <jitendra.simformsolutions@gmail.com>
2022-08-17 18:32:25 +0530

    addded

--- app/src/main/java/com/example/simformgitexcercisenew/MainActivity.kt
+++ app/src/main/java/com/example/simformgitexcercisenew/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO:  12/12/12
+        // TODO:  12/12/12 asdadasd
     }
 }
